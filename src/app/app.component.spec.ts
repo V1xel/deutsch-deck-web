1e8a4a320da7f9b33b7b4e027f9339140cdaaba2
Vitalii Koliesnik <v1xel@outlook.de>
2023-12-21 15:52:48 +0100

    Refactoring: Clear app component
    commit 5f5adbb87238a89533e8a8ea07bf4994f307d3de
    Author: Vitalii Koliesnik <v1xel@outlook.de>
    Date:   Thu Dec 21 15:45:55 2023 +0100
    
        Updated app component title
    
    commit 6c2ec0fdc8b1b3d0a7707b7dd8397482a13456ba
    Author: Vitalii Koliesnik <v1xel@outlook.de>
    Date:   Thu Dec 21 15:45:05 2023 +0100
    
        Removed deprecated test
    
    commit 11f883cefc0e37848abb89e893808a34f8aeccd9
    Author: Vitalii Koliesnik <v1xel@outlook.de>
    Date:   Thu Dec 21 15:40:17 2023 +0100
    
        Removed unnecessary files

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,9 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'deutsch-deck-web' title`, () => {
+  it(`should have the 'deutsch-deck' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('deutsch-deck-web');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, deutsch-deck-web');
+    expect(app.title).toEqual('deutsch-deck');
   });
 });
